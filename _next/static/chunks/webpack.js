9a0efbf51b4718d7a5271d995e5d547bd7b4de54
Khathawut.C <chaipraphun.k@gmail.com>
2021-04-24 20:14:52 +0700

    update version 1.0.5

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b6bcbc620ad827f5d4ac";
+/******/ 	var hotCurrentHash = "3df32062b0e5e36bfdaa";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
